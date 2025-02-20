4b4ba13e1161ebd49097e310b01e159488e6093b
Vladislav Chubrikov <vladchubrikov@dev.com>
2025-02-20 18:52:30 +0300

    init: prepare app template with wp

---
+++ webpack.config.ts
@@ -0,0 +1,48 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+import type { Configuration } from "webpack";
+
+interface EnvOptions {
+  mode: "development" | "production";
+  port: number;
+}
+
+export default (env: EnvOptions) => {
+  const isDev = env.mode === "development";
+
+  const config: Configuration = {
+    mode: env.mode ?? "development",
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].[contenthash].js",
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+    devtool: isDev && "inline-source-map",
+    devServer: isDev && {
+      port: env.port ?? 3000,
+      open: true,
+    },
+  };
+
+  return config;
+};
